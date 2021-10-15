814ae3dbe70101221b46085b5d2cf061dc322a99
Ângelo dos Santos Melo <meloas@saude.sc.gov.br>
2021-10-15 10:34:47 -0300

    add routes react-hook-form and initial pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
